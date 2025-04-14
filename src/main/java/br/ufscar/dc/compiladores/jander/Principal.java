299f1e259a325574efc960db08bda065861e8851
CassaX <matheuscassatti@estudante.ufscar.br>
2025-04-14 09:53:44 -0300

    Update Principal.java
    Atualizado para a geração dos tokens e escrita no arquivo de saída

--- src/main/java/br/ufscar/dc/compiladores/jander/Principal.java
+++ src/main/java/br/ufscar/dc/compiladores/jander/Principal.java
@@ -16,15 +16,29 @@ import java.io.IOException;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Principal {
 
     public static void main(String[] args) throws IOException {
-        CharStream cs = CharStreams.fromFileName(args[0]);
-        Jander jander = new Jander(cs);
-        while(jander.nextToken().getType() != Token.EOF){
-            System.out.print("");
-        }
+  
+        
+        try{
+            CharStream cs = CharStreams.fromFileName(args[0]);
+            Jander jander = new Jander(cs);
             
+            String arquivoSaida = args[1];
+            PrintWriter pw = new PrintWriter(arquivoSaida);
+
+            Token t = null;
+
+            while((t = jander.nextToken()).getType() != Token.EOF){
+                pw.println("<" + Jander.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
+            }
+            pw.close();
+        }catch(IOException ex){
+            
+        }
     }
 }
